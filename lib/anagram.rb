3a6c7fdecd34947d4af20abc04098320fee21c8a
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-14 17:49:22 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :match
+  
+  def initialize (word)
+    @word = word 
+  end
+  
+  def match(challenge)
+    challenge.select do |str| 
+    check=str.split("").sort 
+    check == @word.split("").sort
+    end
+  end 
+end
