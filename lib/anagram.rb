458354c7ef18bf0f60273dc4167ee01d86112ae6
anthony mcmullen <jondouglasheit@gmail.com>
2020-04-09 12:55:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 require 'pry'
+=======
+>>>>>>> e8755ef72ac01cbceecf8d3d07103a2b2b8267e9
 class Anagram
   attr_accessor :match
   
@@ -6,10 +9,20 @@ class Anagram
     @word = word 
   end
   
+<<<<<<< HEAD
   def match(challenge)
     challenge.select do |str| 
     check=str.split("").sort 
     check == @word.split("").sort
     end
   end 
+=======
+  def match (word_check) #match
+  match = []
+  word_long= word_check.sort 
+  word_long.map! do |x| match<< word_check if word_check == @word
+  end 
+ end  
+
+>>>>>>> e8755ef72ac01cbceecf8d3d07103a2b2b8267e9
 end
